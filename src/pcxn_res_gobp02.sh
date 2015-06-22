78d05e8bdb39fe9319913e37ef5ec0b35dbde85e
yeredh <yered.h@gmail.com>
2015-06-22 15:11:29 -0400

    Scripts to aggregate the experiment-level correlation estimates and p-values into matrices. Meant to be run on Odyssey.

---
+++ src/pcxn_res_gobp02.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+#SBATCH -J pcxn_gobp_res # A single job name for the array 
+#SBATCH -n 16 # Number of cores 
+#SBATCH -N 1 # All cores on one machine 
+#SBATCH -p serial_requeue,irizarry # Partition 
+#SBATCH --mem-per-cpu 20000 # Memory request 
+#SBATCH -t 0-04:00 # (D-HH:MM) 
+#SBATCH -o /net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/log/pcxn_gobp_res_%a.out # Standard output 
+#SBATCH -e /net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/log/pcxn_gobp_res_%a.err # Standard error   
+module load centos6/R-3.1.1 
+R CMD BATCH "--args ${SLURM_ARRAY_TASK_ID}" /net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/src/pcxn_res_gobp02a.R /net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/log/pcxn_gobp_res"${SLURM_ARRAY_TASK_ID}".Rout
---
+++ src/pcxn_res_gobp02a.R
@@ -0,0 +1,64 @@
+rm(list=ls())
+# ==== GSE annotation ====
+gse_annot = readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/data/GSE_annotation.RDS")
+# get sample size per GSE series
+gse_count = table(gse_annot$GSE)
+gse_count = sort(gse_count,decreasing=T)
+# keep series with at least 10 samples
+gse_ids = names(gse_count[gse_count >= 5])
+
+# ==== Pathway Annotation ====
+gs_lst = readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/data/gobp_gs.RDS")
+n_pairs = choose(length(gs_lst),2)
+
+library(parallel)
+nc = detectCores()
+
+# split GSE into four groups
+gse_split = split(gse_ids, ceiling(seq_along(gse_ids)/421))
+
+# read command line arguments
+# the script is run as job array, the args correponds to the job array index
+args <- as.numeric(commandArgs(trailingOnly = T))
+
+
+# get correlation estimates and p-values
+r_mat = matrix(NA,ncol=length(gse_split[[args]]),nrow=n_pairs,dimnames=list(NULL,gse_split[[args]]))
+p_mat = r_mat
+
+
+ic=1
+pb = txtProgressBar(min=0,max=length(gse_split[[args]]),style=3,initial=0)
+for(gse in gse_split[[args]]){
+  # load RDS file
+  gse_lst = readRDS(paste0("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/GOBP/",gse,"_gobp_pathcor.RDS"))
+  # get correlation estimates
+  tmp = mclapply(gse_lst,function(x){
+    if(length(x) == 7){
+      return(x[[3]])
+    }else{
+      return(NA)
+    }},mc.cores=nc)
+  
+  tmp = simplify2array(tmp, higher = TRUE)
+  r_mat[,ic] = tmp
+  
+  # get p-values
+  tmp = mclapply(gse_lst,function(x){
+    if(length(x) == 7){
+      return(x[[6]])
+    }else{
+      return(NA)
+    }},mc.cores=nc)
+  
+  tmp = simplify2array(tmp, higher = TRUE)
+  p_mat[,ic] = tmp
+  
+  setTxtProgressBar(pb,ic)
+  ic=ic+1
+}
+close(pb)
+saveRDS(p_mat,paste0("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/GOBP/res/p_mat",args,".RDS"))
+saveRDS(r_mat,paste0("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/GOBP/res/r_mat",args,".RDS"))
+
+rm(list=ls())
