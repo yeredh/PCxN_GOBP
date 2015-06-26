155ef16f58ba0405ed29e96d6063daa0f0988f37
yeredh <yered.h@gmail.com>
2015-06-25 20:39:42 -0400

    Script to check for missing experiment-level estimates. Meant to be run on Odyssey.

---
+++ src/pcxn_gse_missing_merge.R
@@ -0,0 +1,17 @@
+# gse_annot = readRDS("Hide Lab/PCxN/data/GSE_annotation.RDS")
+gse_annot = readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/data/GSE_annotation.RDS")
+# get sample size per GSE series
+gse_count = table(gse_annot$GSE)
+gse_count = sort(gse_count,decreasing=T)
+# keep series with at least 5 samples
+gse_ids = names(gse_count[gse_count >= 5])
+
+# read RDS files with results
+setwd("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge")
+rds_files = sort(list.files(pattern="RDS"))
+gse_done = sort(sapply(rds_files,function(x){gsub("(GSE[0-9]*)_cp_gobp_pathcor.*","\\1",x)}))
+names(gse_done) = NULL
+gse_miss=setdiff(gse_ids,gse_done)
+ind=match(gse_miss,gse_ids)
+ind
+
