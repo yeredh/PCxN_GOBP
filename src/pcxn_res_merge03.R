fa7b7651c00e6b8250dc4bb57cd31fcad4a1845f
yeredh <yered.h@gmail.com>
2015-06-25 20:42:41 -0400

    Script to estimate the weighted average of the experiment-level correlation estimates.

---
+++ src/pcxn_res_merge03.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+# ==== Experiment-level correlation estimates ====
+r_mat = cbind(readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat1.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat2.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat3.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat4.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat5.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat6.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat7.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat8.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat9.RDS"),
+              readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_mat10.RDS"))
+
+# sample size per experiment
+n_vec = readRDS("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/n_vec.RDS")
+
+# weighted average for the correlation estimates
+n_mult = n_vec/sum(n_vec)
+rm(n_vec)
+r_bar = r_mat%*%n_mult
+
+saveRDS(r_bar,paste0("/net/hsphfs1/srv/export/hsphfs1/share_root/hide_lab/PCxN/output/merge/res/r_bar.RDS"))
+
